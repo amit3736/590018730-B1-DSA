6c6912c3436571044fb3c08b9a621b74ab3a7662
amit3736 <aranjan946@gmail.com>
2024-09-27 00:41:08 +0530

    stack_using_array

---
+++ StackUsingArray.c
@@ -0,0 +1,49 @@
+// Stack Implementation using an array
+
+#include<stdio.h>
+#include<conio.h>
+#define MAX 5
+
+int stack[MAX];
+int top = -1;
+
+void push(int data) {
+  if (top == MAX - 1) {
+
+    printf("Stack Overflow \n");
+  }
+  stack[++top] = data;
+  printf("%d pushed to stack\n", data);
+}
+
+void pop() {
+
+  if (top == -1) {
+    printf("Stack Underflow\n");
+    return;
+  }
+
+  printf("%d popped from stack\n", stack[top--]);
+}
+
+void display() {
+  if (top == -1) {
+    printf("Stack is empty\n");
+    return;
+  }
+  printf("Stack element: ");
+  for (int i = top; i >= 0; i--) {
+    printf("%d ", stack[i]);
+  }
+  printf("\n");
+}
+
+int main() {
+  push(4);
+  push(6);
+  push(30);
+  display();
+  pop();
+  display();
+  return 0;
+}
